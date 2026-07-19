fee5381a91cf875318483598b32f43c56d32d49e
guna2007 <lankalapalligunapersonal@gmail.com>
2026-07-19 10:01:12 +0530

    feat(auth): implement authentication workflow

---
+++ SmartBank-Management-System/src/main/java/com/bank/smartbank/dto/auth/LoginResponse.java
@@ -0,0 +1,19 @@
+package com.bank.smartbank.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+
+	private String token;
+	private String tokenType = "Bearer";
+	private Long userId;
+	private String email;
+	private String fullName;
+	private String role;
+
+}
